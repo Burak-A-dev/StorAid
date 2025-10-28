3a9bec9de60c3356a42161d31218a3894a5ba3b1
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-10-28 23:37:09 +0100

    initial commit of the footer component.

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='flex justify-around bg-green-950 text-white h-12 text-center p-3'>
+        <div className="">
+        <h4 className=''>© 2025 StorAid, All rights reserved.</h4>
+    </div>
+    <div className="flex gap-5">
+        <a href="" className="">Terms & Conditions</a>
+        <a href="" className="">Privacy Policy</a>
+    </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Footer from './components/common/Footer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App/>
+    <Footer/>
   </StrictMode>,
 )
