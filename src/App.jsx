6af7b0d5c4df176d2411c9d460834050793d0b44
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-11-01 22:24:49 +0100

    App.jsx code cleanup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
-import Hero from "./components/common/Hero";
-import Navbar from "./components/common/Navbar";
-
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Hero
-  h3="Welcome to StorAid"
-  h1="Space Simplified, Storage Perfected"
-  p="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a sem magna. Etiam ac odio sit amet lorem fringilla sodales. Cras lobortis, libero at iaculis luctus, nisi ex pellentesque nisi, at euismod sem ipsum ac dolor."
-  img="/hero-image.jpg"
-  
-/>
     </div>
   );
 }
