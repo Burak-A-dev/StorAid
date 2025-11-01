e866de787e1a58dca1f2427ecc9e84e625cb2810
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-11-01 22:20:21 +0100

    Hero component finished +  changes made to the CTAButton for better usage.

--- src/components/common/CTAButton.jsx
+++ src/components/common/CTAButton.jsx
@@ -1,10 +1,11 @@
-export default function CTAButton({ text, onClick, type="button" }) {
+export default function CTAButton({ text, onClick, type = "button", className = "" }) {
   return (
     <button
-      className='bg-amber-400 text-green-950 text-sm font-medium rounded-md flex items-center justify-center px-6 py-3 cursor-pointer'
+      className={`bg-amber-400 text-green-950 text-sm font-medium rounded-md flex items-center justify-center px-6 py-3 cursor-pointer ${className}`}
       onClick={onClick}
-      type={type}>
+      type={type}
+    >
       {text}
     </button>
-  )
+  );
 }
---
+++ src/components/common/Hero.jsx
@@ -0,0 +1,24 @@
+import CTAButton from "./CTAButton";
+import icon from '../../assets/Hero-Silhouette.svg';
+
+export default function Hero({ h3, h1, p, showButton = true }) {
+  return (
+    <div className="bg-[#365349] h-120 px-20 flex flex-row align-center justify-around items-center">
+      <div className="flex flex-col gap-2 w-110">
+        {h3 && <h3 className="text-amber-400 font-medium">{h3}</h3>}
+        {h1 && <h1 className="text-emerald-50 text-5xl font-bold">{h1}</h1>}
+        {p && <p className="text-white text-sm font-light">{p}</p>}
+        {/*ChatGPT användes för att ta reda på hur CTAButton kan visas om den behövs.*/}
+        {showButton && <CTAButton text="Discover More" className="self-start" />}
+      </div>
+      
+      <div className="self-end w-80">
+        <img
+          className=""
+          src={icon}
+          alt="Silhouette of a person"
+        />
+      </div>
+    </div>
+  );
+}
