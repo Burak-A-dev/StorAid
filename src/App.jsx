6f9c4f87f4e392fcad5a0174b7da21bef42caa9b
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-11-05 20:00:47 +0100

    code removal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import AboutUs from "./components/common/AboutUs";
-import Services from "./components/common/Services";
-
 function App() {
   return (
     <div>
-      {/* <Services/> */}
-      <AboutUs/>
     </div>
   );
 }
