3e39093d61032b754408b76b6eb8aa8d4bcde1d9
Burak-A-dev <alpay.ozdemirok@gmail.com>
2025-11-06 20:13:02 +0100

    TestimonialsCard component completed.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ function App() {
     <div>
       <TestimonialsCard
         name="Carrisa Jocelyn"
-        role="Customer"
-        review="Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante."
-        rating={5}
-        avatar=""
+        comment="Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ut libero lectus, porta nec turpis sit amet, lobortis fringilla ante."
+        rating={1}
+        companyName="Apple"
       />
     </div>
   );
